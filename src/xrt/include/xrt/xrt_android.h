1aed10f998581906a499317ce87d6f0d5876998c
Ryan Pavlik <ryan.pavlik@collabora.com>
2021-03-26 18:47:14 -0500

    xrt: Describe Android lifecycle callbacks

--- src/xrt/include/xrt/xrt_android.h
+++ src/xrt/include/xrt/xrt_android.h
@@ -20,6 +20,7 @@ extern "C" {
 
 struct _JavaVM;
 struct _ANativeWindow;
+struct xrt_instance;
 
 /*!
  * Distinguishes the possible Android surface events from each other.
@@ -41,6 +42,31 @@ typedef bool (*xrt_android_surface_event_handler_t)(struct xrt_instance *xinst,
                                                     struct _ANativeWindow *window,
                                                     enum xrt_android_surface_event event,
                                                     void *userdata);
+
+/*!
+ * Distinguishes the possible Android lifecycle events from each other.
+ *
+ * Used as a bitmask when registering for callbacks.
+ */
+enum xrt_android_lifecycle_event
+{
+	XRT_ANDROID_LIVECYCLE_EVENT_ON_CREATE = 1 << 0,
+	XRT_ANDROID_LIVECYCLE_EVENT_ON_DESTROY = 1 << 1,
+	XRT_ANDROID_LIVECYCLE_EVENT_ON_PAUSE = 1 << 2,
+	XRT_ANDROID_LIVECYCLE_EVENT_ON_RESUME = 1 << 3,
+	XRT_ANDROID_LIVECYCLE_EVENT_ON_START = 1 << 4,
+	XRT_ANDROID_LIVECYCLE_EVENT_ON_STOP = 1 << 5,
+};
+
+/*!
+ * A callback type for a handler of Android lifecycle events.
+ *
+ * Return false to be removed from the callback list.
+ */
+typedef bool (*xrt_android_lifecycle_event_handler_t)(struct xrt_instance *xinst,
+                                                      struct _ANativeWindow *window,
+                                                      enum xrt_android_lifecycle_event event,
+                                                      void *userdata);
 
 #ifdef XRT_OS_ANDROID
 
