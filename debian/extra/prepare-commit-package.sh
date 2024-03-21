5eaa252ffd50554c5e20e1feef85f50ad4afce50
Rylie Pavlik <rylie.pavlik@collabora.com>
2024-03-21 09:52:43 -0500

    Start with packaging from Bullseye

---
+++ debian/extra/prepare-commit-package.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+# Copyright 2020-2022, Rylie Pavlik <rylie@ryliepavlik.com>
+# SPDX-License-Identifier: CC0-1.0
+
+# Packages produced this way are for automated use only and shouldn't be uploaded to the Debian archive.
+
+set -e
+(
+    cd "$(dirname $0)"
+    cd ../..
+    export DEVSCRIPTS_CHECK_DIRNAME_LEVEL=0
+
+    if [ x"$1" != x ]; then
+        COMMIT_TO_PACKAGE=$1
+        echo "Package version will describe commit specified on command line: ${COMMIT_TO_PACKAGE}"
+        export COMMIT_TO_PACKAGE
+    else
+        COMMIT_TO_PACKAGE=main
+        echo "Package version will describe default commit: ${COMMIT_TO_PACKAGE}"
+        export COMMIT_TO_PACKAGE
+    fi
+
+    if [ x"$2" != x ]; then
+        PKG_REVISION=$2
+        echo "Appending custom revision suffix specified on command line: ${PKG_REVISION}"
+        export PKG_REVISION
+    else
+        PKG_REVISION=1~bpo11~ci$(date --utc "+%Y%m%d")
+        echo "Appending auto-generated revision suffix: ${PKG_REVISION}"
+        export PKG_REVISION
+    fi
+
+    UPSTREAM_VER=$(git describe --exclude "v0*" "$COMMIT_TO_PACKAGE" | sed -E -e 's/^v//' -e 's/-([0-9]+)-g([0-9a-f])/+git\1.\2/')
+    echo "Computed package version ${UPSTREAM_VER}"
+    git archive -o "../monado_${UPSTREAM_VER}.orig.tar.gz" ${COMMIT_TO_PACKAGE}
+    dch --newversion "${UPSTREAM_VER}-${PKG_REVISION}" --preserve "Automated CI build of commit ${COMMIT_TO_PACKAGE}"
+)
---
+++ debian/extra/register-dpkg-mergechangelogs.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+# Copyright 2020, Rylie Pavlik <rylie@ryliepavlik.com>
+# SPDX-License-Identifier: CC0-1.0
+# Registers dpkg-mergechangelogs as a git merge driver so the gitattributes in here works right.
+# defaults to local-only: calling with --global is recommended
+set -e
+git config "$@" merge.dpkg-mergechangelogs.name="debian/changelog merge driver"
+git config "$@" merge.dpkg-mergechangelogs.driver="dpkg-mergechangelogs -m %O %A %B %A"
