a36ab4f1b6a7dfaf5477dde21a892d15a584384a
Gavin John <>
2024-03-28 13:29:48 -0500

    scripts/run_iwyu: Add IWYU binary detection

--- scripts/run_iwyu.sh
+++ scripts/run_iwyu.sh
@@ -4,10 +4,15 @@
 
 (
     set -e
+    export iwyu_tool=$(which iwyu_tool 2> /dev/null || which iwyu_tool.py 2> /dev/null)
+    if [ -z "$iwyu_tool" ]; then
+        echo "Error: include-what-you-use not found. Please install it."
+        exit 1
+    fi
     cd "$(dirname $0)" && cd ..
     extra_args=""
     for fn in /usr/share/include-what-you-use/iwyu.gcc.imp /usr/share/include-what-you-use/*intrinsics.imp /usr/share/include-what-you-use/libcxx*.imp; do
         extra_args="${extra_args} -Xiwyu --mapping_file=$fn"
     done
-    iwyu_tool -p build src/xrt/ "$@" -- -Xiwyu "--mapping_file=$(pwd)/scripts/mapping.imp" ${extra_args} | tee iwyu.txt
+    $iwyu_tool -p build src/xrt/ "$@" -- -Xiwyu "--mapping_file=$(pwd)/scripts/mapping.imp" ${extra_args} | tee iwyu.txt
 )
